14c5ce05f087b88f4755e9a265fb9f5fac5023da
David Shan <david.shan@mail.utoronto.ca>
2021-01-12 10:59:52 -0500

    Refactor code a bit - decouple drawing and grid state from one another

--- Cell.js
+++ Cell.js
@@ -4,19 +4,27 @@ class Cell {
         this.offsetX = offsetX;
         this.offsetY = offsetY;
         this.size = 32;
-
-        square(offsetX, offsetY, this.size);
         
         // TODO: error checking
     }
 
+    get isTraversable() {
+        return this.traversable;
+    }
+
     setTraversed(value) {
-        if (value == true) {
+        this.traversable = value;
+    }
+
+    display() {
+        // Display different colour based on traversable and
+        // other variables (yet to be defined)
+        if (this.traversable) {
             fill(255, 255, 255);
         } else {
             fill(0, 0, 255); 
         }
-        this.traversable = value;
+
         square(this.offsetX, this.offsetY, this.size);
     }
 
--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
-currXOffset = 0;
-currYOffset = 0;
-currSize = 32;
+const CELLSIZE = 32;
+const GRIDSIZE = 20;
 
-grid = new Array();
+const grid = new Array();
 
+/* CORE DRAWING FUNCTIONS (p5.js) */
 function setup() {
     createCanvas(2000, 2000);
-
-    for (i = 0; i < 20; i++) {
-        row = new Array();
-        grid.push(row);
-        for (j = 0; j < 20; j++) {
-            posX = currXOffset * currSize;
-            posY = currYOffset * currSize;
-    
-            let square = new Cell(posX, posY, true);
-            row.push(square);
-    
-            currXOffset++;
-        }
-
-        currYOffset++;
-        currXOffset = 0;
-    }
+    initGrid();
 
     grid[1][2].setTraversed(false);
     console.log(grid);
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        const old = grid[1][2].isTraversable;
+        grid[1][2].setTraversed(!old);
+    }
+    displayGrid();
+}
+
+/* HELPER FUNCTIONS */
+function displayGrid() {
+    for (i = 0; i < GRIDSIZE; i++) {
+        for (j = 0; j < GRIDSIZE; j++) {
+            grid[i][j].display();
+        }
+    }
+}
+
+function initGrid() {
+    for (i = 0; i < GRIDSIZE; i++) {
+        row = new Array();
+        grid.push(row);
+
+        posY = i * CELLSIZE;
+
+        for (j = 0; j < GRIDSIZE; j++) {
+            posX = j * CELLSIZE;
+            row.push(new Cell(posX, posY, true));
+        }
+    }
 }
